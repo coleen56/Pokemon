a959bd1ee5835126dc21c958c5b5710ac5464cd3
coleen56 <coleen.conte@orange.fr>
2024-12-02 12:09:30 +0100

    test commit

--- src/TypePlante.java
+++ src/TypePlante.java
@@ -1,9 +1,5 @@
 public class TypePlante extends Pokemon {
     public TypePlante(String nom, int hp, double atk, String type) {
         super(nom, hp, atk, type);
-    }
-    /**
-     * surcharge de la methode attaquer
-     * @param p
-     */
+    } // test
 }
